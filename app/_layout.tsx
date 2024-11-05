b20d0b5bea264882c1ff7588240e6f9b0a299709
Ahmad Maulana R <liezkaal@gmail.com>
2024-11-05 14:34:37 +0700

    ui prep

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,8 +1,29 @@
-import { Slot, Stack } from 'expo-router'
+import { SplashScreen, Stack } from 'expo-router'
+import { useFonts } from 'expo-font'
 import "../global.css";
+import { useEffect } from 'react';
 // import React from 'react'
 
+SplashScreen.preventAutoHideAsync // To keep splashscreen on top until the app is ready
+
 const RootLayout = () => { 
+  const [fontsLoaded, error] = useFonts({
+    "Poppins-Black": require("../assets/fonts/Poppins-Black.ttf"),
+    "Poppins-Bold": require("../assets/fonts/Poppins-Bold.ttf"),
+    "Poppins-ExtraBold": require("../assets/fonts/Poppins-ExtraBold.ttf"),
+    "Poppins-ExtraLight": require("../assets/fonts/Poppins-ExtraLight.ttf"),
+    "Poppins-Light": require("../assets/fonts/Poppins-Light.ttf"),
+    "Poppins-Medium": require("../assets/fonts/Poppins-Medium.ttf"),
+    "Poppins-Regular": require("../assets/fonts/Poppins-Regular.ttf"),
+    "Poppins-SemiBold": require("../assets/fonts/Poppins-SemiBold.ttf"),
+    "Poppins-Thin": require("../assets/fonts/Poppins-Thin.ttf"),
+  });
+  useEffect(() => { // Like an observer
+    if (error) throw error
+    if (fontsLoaded) SplashScreen.hideAsync // The app is ready, splashscreen akan disembunyikan
+  }, [fontsLoaded, error]) // Observing these 
+  
+  if (!fontsLoaded && !error) return null
   return (
     <Stack>
       <Stack.Screen name="index" options={ {headerShown: false} } />
--- app/index.tsx
+++ app/index.tsx
@@ -4,12 +4,12 @@ import { StatusBar } from "expo-status-bar";
 import '../global.css'
 
 
-export default function RootLayout() {
+export default function App() {
   return (
     <View className="flex-1 justify-center items-center bg-white">
-      <Text className="text-3xl">Aora!</Text>
+      <Text className="text-3xl font-pblack">Aora!</Text>
       <StatusBar style="auto"/>
-      <Link href="/profile" style={{color: 'blue'}}>Go to Profile</Link>
+      <Link href="./home" style={{color: 'blue'}}>Go to Home</Link>
     </View>
   );
 }
